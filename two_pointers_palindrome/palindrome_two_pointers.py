85d4ccad841263338c3d3cf974004ab81af1d982
Loading... <129302570+Ilyas-00@users.noreply.github.com>
2025-04-08 20:14:45 +0200

    Update palindrome_two_pointers.py

--- two_pointers_palindrome/palindrome_two_pointers.py
+++ two_pointers_palindrome/palindrome_two_pointers.py
@@ -13,3 +13,18 @@ def is_palindrome(s):
         right -= 1
         
     return True
+
+def main():
+
+    test_cases = ["RACEACAR", "A", "ABCDEFGFEDCBA",
+                  "ABC", "ABCBA", "ABBA", "RACEACAR"]
+    for i in range(len(test_cases)):
+        print("Test Case #", i + 1)
+        print("-" * 100)
+        print("The input string is '", test_cases[i], "' and the length of the string is ", len(test_cases[i]), ".", sep='')
+        print("Is it a palindrome?.....", is_palindrome(test_cases[i]))
+        print("-" * 100)
+
+
+if __name__ == '__main__':
+    main()
